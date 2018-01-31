af81d2c97dfb6a24d807ca17c9d2513862fbda9e
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2018-01-31 15:27:55 +0100

    Creata pagina di grazie di Solede

---
+++ pages/grazie.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+<div class="has-text-centered grazie">
+    <div class="title is-1">Grazie Per Averci contattati</div>
+    <div class="title is-3">Ti risoponderemo al più presto.</div>
+
+    <div class="title is-4">
+        <a href="/">Torna alla home</a>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+.grazie {
+    margin: 0rem 0rem 10rem 0rem;
+}
+</style>
