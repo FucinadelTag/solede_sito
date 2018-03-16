0ba6f8222a9a4b67f4e803b72fe0f4d32857c5ba
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2018-03-16 18:14:41 +0100

    Rimosso SMG

--- layouts/default.vue
+++ layouts/default.vue
@@ -105,14 +105,6 @@
 
                         </div>
                         <br>
-                        <div class="has-text-centered">
-                            <i>In partnership con:</i>
-                            <br>Script Media Group Ltd
-                            <br> 90 Leinster Road
-                            <br>Rathmines Dublin 6
-                            <br>Vat IE​3458266BH
-                        </div>
-                        <br>
                         <div>
                                 <a href="/privacy" title="Privacy Policy">Privacy Policy</a>
                         </div>
